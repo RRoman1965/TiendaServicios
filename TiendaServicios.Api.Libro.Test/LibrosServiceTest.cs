f7b8ffc8b75209427f43b91785af1d7ad7ef3ba0
Roberto Roman <roman_roberto@hotmail.com>
2021-01-20 19:07:47 +0100

    Prueba cambios proyecto test

--- TiendaServicios.Api.Libro.Test/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Test/LibrosServiceTest.cs
@@ -59,7 +59,7 @@ namespace TiendaServicios.Api.Libro.Test
         [Fact]
         public async void GuardarLibro()
         {
-            System.Diagnostics.Debugger.Launch();
+            //Este test es para unit test libro
 
             var options = new DbContextOptionsBuilder<ContextoLibreria>()
                 .UseInMemoryDatabase(databaseName: "BaseDatosLibro")
